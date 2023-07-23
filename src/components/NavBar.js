18a7949addb1d9c8236cbf1694e23e29911ed904
Nayara Nasaria Soares da Silva <nayaranasaria@MacBook-Pro-de-Nayara.local>
2023-07-23 16:44:19 -0300

    Primeiro Componente

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+const NavBar = () => {
+
+    return(
+        <div className="NavBar">
+        <header className="NavBar__style">
+        <ul className="itens__navbar">
+          <li>Home</li>
+          <li>Produtos</li>
+          <li>Sobre</li>
+        </ul>
+      </header>
+        </div>
+    )
+}
+
+export default NavBar;
